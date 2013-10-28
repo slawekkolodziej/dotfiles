159fc87b953849e903bedfddc8a3a552121c70ec
Slawek Kolodziej <hfrntt@gmail.com>
2013-10-28 16:31:32 +0100

    Disable guest account, close #2

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -314,8 +314,7 @@ ln -s /Applications/Xcode.app/Contents/Applications/iPhone\ Simulator.app ~/Appl
 ###############################################################################
 
 # Disable guest access
-defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
-defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
+sudo defaults write /Library/Preferences/com.apple.loginwindow.plist GuestEnabled 0
 
 # Set Illustrator default language, probably this has to be run after installing illustrator
 defaults write com.adobe.Illustrator AppleLanguages '("pl-PL")'
