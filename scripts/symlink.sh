7c3ce7f8de014ae65b9004b9b92a132fcad3f806
Slawek Kolodziej <hfrntt@gmail.com>
2013-10-29 00:08:30 +0100

    Symlink ST3 config, close #3

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,21 +1,34 @@
+#!/bin/bash
+
 function symlink() {
   rsync \
     --exclude ".git/" \
-    --exclude "scripts/" \
+    --exclude ".gitmodules" \
     --exclude ".DS_Store" \
+    --exclude "apps/" \
+    --exclude "casks/" \
+    --exclude "scripts/" \
+    --exclude "README.md" \
     --exclude "init.sh" \
-    --exclude "README.md" -av --no-perms . ~
-  # source ~/.bash_profile
+    -av --no-perms "$1" "$2"
 }
 
+function symlink_all() {
+  git submodule install
+  symlink . ~
+  symlink ./apps/sublime3/ ~/Library/Application\ Support/Sublime\ Text\ 3/
+}
+
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  symlink
+  symlink_all
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    symlink
+    symlink_all
   fi
 fi
 
 unset symlink
+unset symlink_all
