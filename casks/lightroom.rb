f3a5d4191ec7ff5226aacf848ba9a200313a9e4c
Slawek Kolodziej <hfrntt@gmail.com>
2013-11-04 23:17:52 +0100

    Add Lightroom 3.6 cask

---
+++ casks/lightroom.rb
@@ -0,0 +1,6 @@
+class Lightroom < Cask
+  url 'http://download.adobe.com/pub/adobe/lightroom/mac/3.x/Lightroom_3_LS11_mac_3_6.dmg'
+  version '3.6'
+  sha1 'a585c781512a5e6ad374a20064bfa07d78f1437d'
+  install 'Adobe Lightroom 3.pkg'
+end
