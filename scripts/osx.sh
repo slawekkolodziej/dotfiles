907c400ad353cfb40c14a7c07fb7ec26e0ff3915
Slawek Kolodziej <hfrntt@gmail.com>
2014-10-18 20:10:23 +0200

    Speed up quick look animation

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -276,6 +276,9 @@ fi
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
+# Speed up Quick Look animation
+defaults write -g QLPanelAnimationDuration -float 0.1
+
 # Don’t group windows by application in Mission Control
 # (i.e. use the old Exposé behavior instead)
 defaults write com.apple.dock expose-group-by-app -bool false
