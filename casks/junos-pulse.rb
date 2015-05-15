acf4c0900829afca9581b7ffcfad0edee1efc7d6
Slawek Kolodziej <hfrntt@gmail.com>
2015-05-15 08:07:13 +0200

    Remove legacy casks

--- casks/junos-pulse.rb
+++
@@ -1,6 +0,0 @@
-class JunosPulse < Cask
-  url 'http://www.engr.psu.edu/sslvpn/installer/JunosPulse.dmg'
-  version '4.0'
-  sha1 'c153e91cbd4f202b7d510c2653da8fce688f96a1'
-  install 'JunosPulse.mpkg'
-end
--- casks/network-connect.rb
+++
@@ -1,6 +0,0 @@
-class NetworkConnect < Cask
-  url 'http://software.ucsf.edu/downloads/vpn/NetworkConnect.dmg'
-  version ''
-  sha1 '68683dfc2d5fd25129ba4c57e3036dfbc054ae8b'
-  install 'NetworkConnect.pkg'
-end
